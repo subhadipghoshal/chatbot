22c859a09d71abd0ced742f9886c1fb3dad94e0d
Subhadip Ghoshal <sghoshal18@gmail.com>
2018-03-26 13:08:08 +0530

    Adding js event listeners after document is ready

--- chatbot/chat/static/js/script.js
+++ chatbot/chat/static/js/script.js
@@ -1,60 +1,63 @@
-$('button').on('click', function(){
-    addMessage();
+$(document).ready(function() {
+    $('button').on('click', function(){
+        addMessage();
+    });
+
+    $(document).on('keydown', function(evt){
+        if (evt.keyCode == 13) {
+          addMessage();
+        }
+    })
+
+    function createMessage(val, side) {
+        var chatMsg = '<li class="chats-' + side + ' pre">';
+        chatMsg += val;
+        chatMsg += '</li>';
+        return chatMsg;
+    }
+
+    function clearPre() {
+        setTimeout(function(){
+            $('.pre').removeClass('pre');
+        }, 0);
+    }
+
+    function scroll() {
+        clearPre();
+        $('.chats').stop().animate({
+            scrollTop: $('.chats')[0].scrollHeight
+        }, 500, function(){
+        });
+    }
+
+    function addMessage() {
+        var val = $('input').val();
+        $('.chats').append(createMessage(val, 'right'));
+        scroll();
+        var data = {
+            'content': val
+        };
+
+        var request = {
+            "async": true,
+            "url": "/response",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(data)
+        }
+
+        $.ajax(request).done(function (response) {
+            console.log(response);
+            $('.chats').append(
+                createMessage(response,'left')
+            );
+            scroll();
+        });
+        $('input').val('');
+    }
+
+    scroll();
 });
 
-$(document).on('keydown', function(evt){
-    if (evt.keyCode == 13) {
-      addMessage();
-    }
-})
-
-function createMessage(val, side) {
-    var chatMsg = '<li class="chats-' + side + ' pre">';
-    chatMsg += val;
-    chatMsg += '</li>';
-    return chatMsg;
-}
-
-function clearPre() {
-    setTimeout(function(){
-        $('.pre').removeClass('pre');
-    }, 0);
-}
-
-function scroll() {
-    clearPre();
-    $('.chats').stop().animate({
-        scrollTop: $('.chats')[0].scrollHeight
-    }, 500, function(){
-    });
-}
-
-function addMessage() {
-    var val = $('input').val();
-    $('.chats').append(createMessage(val, 'right'));
-    scroll();
-    var data = {
-        'content': val
-    };
-
-    var request = {
-        "async": true,
-        "url": "/response",
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "data": JSON.stringify(data)
-    }
-
-    $.ajax(request).done(function (response) {
-        console.log(response);
-        $('.chats').append(
-            createMessage(response,'left')
-        );
-        scroll();
-    });
-    $('input').val('');
-}
-
-scroll();
